c746108730c47dab32968eb776e8ff31ceb44bd3
Alex <thealexsb@gmail.com>
2022-12-22 00:03:42 +0300

    remove zigbee scripts

--- exporter/zigbee-get.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3 -B
-#coding: UTF-8
-import telegram, memcache, cgi, json
-
-print("Content-type: text/plain")
-print('')
-
-mc = memcache.Client(["127.0.0.1:11211"])
-print(mc.get('zigbee-devices'))
