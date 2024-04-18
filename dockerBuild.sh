aa9c3d3a290bb9a420cc90c35b1348332f42ffe4
Alex <thealexsb@gmail.com>
2024-04-18 22:00:55 +0300

    update docker build

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+if [[ $DOCKER_REGISTRY == "" ]];
+  then
+    echo "specifiy DOCKER_REGISTRY env"
+    exit
+  else
+    echo "using registry ${DOCKER_REGISTRY}"
+fi
+
+TAG=`git rev-parse HEAD`
+DATE=`date +%s`
+
 set -x
 #docker buildx create --name weather-station-builder --use --config ./buildkit.toml
 docker buildx use weather-station-builder
-docker buildx build --platform=linux/arm/v7 --progress=plain -t bee:5001/weather-station:latest --push .
+docker buildx build --platform=linux/arm/v7 --progress=plain -t ${DOCKER_REGISTRY}/weather-station:latest -t ${DOCKER_REGISTRY}/weather-station:${DATE}_${TAG} --push .
 docker buildx use default
