67b000593965da2ab871ab9f6030db1d48f2e6b2
Alex <thealexsb@gmail.com>
2024-04-21 10:17:26 +0300

    docker build: stop buildkit after use; check if it already exists

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -10,8 +10,17 @@ fi
 TAG=`git rev-parse HEAD`
 DATE=`date +%s`
 
+BUILDX_NAME=weather-station-builder
+
+existingBuildx=`docker buildx ls --format "{{.Name}}" | grep ${BUILDX_NAME}`
+if [[ existingBuildx == "" ]];
+  then
+    docker buildx create --name ${BUILDX_NAME} --use
+  else
+    docker buildx use ${BUILDX_NAME}
+fi
+
 set -x
-#docker buildx create --name weather-station-builder --use --config ./buildkit.toml
-docker buildx use weather-station-builder
 docker buildx build --platform=linux/arm/v7 --progress=plain -t ${DOCKER_REGISTRY}/weather-station:latest -t ${DOCKER_REGISTRY}/weather-station:${DATE}_${TAG} --push .
 docker buildx use default
+docker buildx stop ${BUILDX_NAME}
